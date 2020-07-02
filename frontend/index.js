b7c2ae4fea2f10d36411c403f2b048b1693d13de
ohbarye <over.rye@gmail.com>
2020-07-02 22:35:45 +0900

    Alert unless metamask

--- frontend/index.js
+++ frontend/index.js
@@ -9,9 +9,7 @@ if (typeof web3 !== "undefined") {
   App.web3Provider = web3.currentProvider;
   web3 = new Web3(web3.currentProvider);
 } else {
-  // If no injected web3 instance is detected, fallback to Ganache.
-  App.web3Provider = new web3.providers.HttpProvider("http://127.0.0.1:7545");
-  web3 = new Web3(App.web3Provider);
+  alert("You need to install Metamask!");
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
