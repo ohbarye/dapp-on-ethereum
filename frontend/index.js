3dbdf655e120ce54ddbc1db3abab6642c3e7dbcf
ohbarye <over.rye@gmail.com>
2020-07-03 00:01:52 +0900

    call contract

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,23 @@
-import "babel-polyfill";
+import "babel-polyfill"; // for https://github.com/ethereum/web3.js/issues/3155
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import MetaCoin from "../build/contracts/MetaCoin.json";
 import Web3 from "web3";
 
 // Is there is an injected web3 instance?
 if (typeof web3 !== "undefined") {
-  App.web3Provider = web3.currentProvider;
-  web3 = new Web3(web3.currentProvider);
+  const web3 = new Web3("ws://localhost:8545");
+
+  // to check if it works
+  web3.eth.getAccounts().then((accounts) => {
+    const metacoinABI = MetaCoin.abi;
+    const metacoinAddress = "0xE97b7eb6AE732248d0FE5BA25A762D436C5dBf22"; // TODO: how to get this dynamically?...unnecessary?
+    const metacoin = new web3.eth.Contract(metacoinABI, metacoinAddress);
+    console.log(metacoin);
+    console.log(accounts);
+    metacoin.methods.getBalanceInEth(accounts[0]).call().then(console.log);
+  });
 } else {
   alert("You need to install Metamask!");
 }
