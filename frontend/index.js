ff3a75ed23dcb7d7de90e950b105675496b92c94
ohbarye <over.rye@gmail.com>
2020-07-02 22:34:06 +0900

    set up frontend web3
    polyfill is necessary for web3.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,18 @@
+import "babel-polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
+import Web3 from "web3";
+
+// Is there is an injected web3 instance?
+if (typeof web3 !== "undefined") {
+  App.web3Provider = web3.currentProvider;
+  web3 = new Web3(web3.currentProvider);
+} else {
+  // If no injected web3 instance is detected, fallback to Ganache.
+  App.web3Provider = new web3.providers.HttpProvider("http://127.0.0.1:7545");
+  web3 = new Web3(App.web3Provider);
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
